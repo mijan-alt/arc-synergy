4dc1f4c73dcc64a53eb1d90d70e63928db4e912c
Awajimijan Igoni <mijanigoni@gmail.com>
2024-08-12 12:53:53 +0100

    working on seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,31 @@ const sans = Albert_Sans({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Arc Synergy",
   description: "Energy Company",
+  openGraph: {
+    title: "Arc Synergy",
+    description: "Energy Company providing services in Africa",
+    url: "https://arc-synergy.vercel.app/",
+    images: [
+      {
+        url: "/assets/Logo.png",
+        width: 800,
+        height: 600,
+        alt: "Arc Synergy Logo",
+      },
+    ],
+    siteName: "Arc Synergy",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@arcsynergy",
+    title: "Arc Synergy",
+    description: "Energy Company providing services in Africa",
+  },
+  alternates: {
+    canonical: "https://arc-synergy.vercel.app/",
+  },
 };
+
 
 export default function RootLayout({
   children,
