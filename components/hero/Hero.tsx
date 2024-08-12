d91e004491c1ab0ccf427cdf9a55ab93498599f5
Awajimijan Igoni <mijanigoni@gmail.com>
2024-08-12 03:19:35 +0100

    hero

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -7,15 +7,12 @@ const Hero = () => {
       {/* Slider */}
       <div className="w-full">
         <div
-          data-hs-carousel='{
-"loadingClasses": "opacity-0"
-    }'
           className="relative z-10"
         >
-          <div className="hs-carousel relative overflow-hidden w-full h-[30rem] md:h-[calc(100vh-106px)]  bg-gray-100  ">
-            <div className="hs-carousel-body absolute top-0 bottom-0 start-0 flex flex-nowrap transition-transform duration-700 opacity-0">
+          <div className=" ">
+            <div className="">
               {/* Item */}
-              <div className="hs-carousel-slide w-full">
+              <div className="w-full">
                 <div className="h-[30rem] md:h-[calc(100vh-106px)]  flex flex-col bg-[url('/assets/hero.png')] bg-cover bg-center bg-no-repeat">
                   <div className="absolute inset-0 bg-black opacity-65"></div>
                   <div className="mt-auto w-full ps-5 pb-5 md:ps-10 md:pb-10 relative z-10">
